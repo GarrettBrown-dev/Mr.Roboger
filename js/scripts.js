632e5b371ced71af5dcf7ea711d36a832d682065
Garrett Brown <gman9mm@live.com>
2020-08-21 08:49:24 -0700

    Added formatting for HTML as well as assets for first variable.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3];
+
+
+
+$(document).ready(function () {
+  $("form#formsubmit").submit(function (event) {
+    event.preventDefault();
+    let userNumber = $("#userinput").val();
+    let arrayOfInput = [];
+    arrayOfInput.push(userWord);
+    const pigLatin = arrayOfInput.map(function (text) {
+      console.log(pigLatin);
+      if (/[aieou]/) {
+        arrayOfInput.push("way");
+        arrayOfInput.join((''));
+      });
