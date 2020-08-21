93a3e9689d9da28437ecbde1a806a7c5087de564
Garrett Brown <gman9mm@live.com>
2020-08-21 14:15:43 -0700

    Continued troubleshooting - unsuccessful.

--- js/scripts.js
+++ js/scripts.js
@@ -9,19 +9,13 @@ $(document).ready(function () {
     //const arrayOfInput = [userNumber];
     for (let i = 0; i <= userNumber; i++) {
       const stringifiedNum = i.toString();
-      if (stringifiedNum.includes("3")) {
+      if (stringifiedNum.includes("3"))
         outputArray.push("Won't you be my neighbor?")
-      } else if (stringifiedNum.includes("2")) {
-        outputArray.push("Boop")
-      } else (stringifiedNum.includes("1")) {
-        outputArray.push("Beep")
-      }
-
+      else if (stringifiedNum.includes("2"))
+        outputArray.push("Boop.");
+      else (stringifiedNum.includes("1"))
+      outputArray.push("Beep");
+      console.log(outputArray)
     }
-    console.log(outputArray)
   });
 });
-
-// Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
-// Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
-// Numbers that contain a 3: all digits are replaced (all digits) with "Won't you be my neighbor?"
