f959cdb5fef68cd864b04fa56f43106d0c9a0161
Garrett Brown <gman9mm@live.com>
2020-08-21 12:49:06 -0700

    First successful condition.

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,21 @@ $(document).ready(function () {
   $("form#formsubmit").submit(function (event) {
     event.preventDefault();
     const userNumber = $("#userNumber").val();
-    const arrayOfInput = [userNumber];
-    console.log(arrayOfInput);
-    const roboWord = arrayOfInput.map(function (text) {
-      let transformedText;
-      // const twoLetters = text.split('')[]
-      // does number in text contain /[1]/
-      if (userNumber.match(/[1]/)) {
-        transformedText = userNumber.shift('');
-        transformedText.push("Beep");
-        console.log("inside first block");
-        return transformedText
-      } else if 
-    });
+    const outputArray = [];
+    //const arrayOfInput = [userNumber];
+    for (let i = 0; i <= userNumber; i++) {
+      const stringifiedNum = i.toString();
+      if (stringifiedNum.includes("3")) {
+        outputArray.push("Won't you be my neighbor?")
+      } else {
+        outputArray.push(stringifiedNum)
+      }
+
+    }
+    console.log(outputArray)
   });
 });
+
+// Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
+// Numbers that contain a 2: all digits are replaced (all digits) with "Boop!"
+// Numbers that contain a 3: all digits are replaced (all digits) with "Won't you be my neighbor?"
