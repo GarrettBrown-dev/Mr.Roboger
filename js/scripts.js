7e4618b9513883226e3f42935ae62a1fa9340fbc
Garrett Brown <gman9mm@live.com>
2020-08-21 14:42:23 -0700

    Researching ideas on how to make conditions not stack.'Break' not working.

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,15 @@ $(document).ready(function () {
     //const arrayOfInput = [userNumber];
     for (let i = 0; i <= userNumber; i++) {
       const stringifiedNum = i.toString();
-      if (stringifiedNum.includes("3"))
+      if (stringifiedNum.includes("3")) {
         outputArray.push("Won't you be my neighbor?")
-      else if (stringifiedNum.includes("2"))
+      } else if (stringifiedNum.includes("2")) {
         outputArray.push("Boop.");
-      else (stringifiedNum.includes("1"))
-      outputArray.push("Beep");
-      console.log(outputArray)
+      } else (stringifiedNum.includes("1")); {
+        outputArray.push("Beep");
+
+        console.log(outputArray)
+      }
     }
   });
 });
