9b6f216a42f7f4542060e1f9974bdc02c187a436
Garrett Brown <gman9mm@live.com>
2020-08-21 09:30:39 -0700

    Restart from beginning due to errors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 const numbers = [1, 2, 3];
 
 
-
 $(document).ready(function () {
   $("form#formsubmit").submit(function (event) {
     event.preventDefault();
     let userNumber = $("#userinput").val();
     let arrayOfInput = [];
-    arrayOfInput.push(userNumber);
-    const roboWord = arrayOfInput.map(function (text) {
-      if (/[1]/) {
-        arrayOfInput.push("Beep");
-        arrayOfInput.join("");
-        console.log(userNumber)
+    arrayOfInput.push(userWord);
+    const pigLatin = arrayOfInput.map(function (text) {
+      console.log(pigLatin);
+      if (/[aieou]/) {
+        arrayOfInput.push("way");
+        arrayOfInput.join((''));
       }
       else if (/[^aieou]/i) {
-        arrayOfInput.replace(/[aieou]/gi, (charAt(0) + "Boop"));
+        arrayOfInput.replace(/[aieou]/gi, (charAt(0) + "ay"));
       }
       else if (/[qu]/gi) {
         $(string).slice(+ "quay");
