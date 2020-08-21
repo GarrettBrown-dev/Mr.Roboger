00280f9addb41f08f632bc5ba03a40b6d580ccec
Garrett Brown <gman9mm@live.com>
2020-08-21 09:52:41 -0700

    Begin testing with different format from previous project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,36 @@
-const numbers = [1, 2, 3];
+const numbers = ["1", "2", "3"];
 
 
 $(document).ready(function () {
   $("form#formsubmit").submit(function (event) {
     event.preventDefault();
-    let userNumber = $("#userinput").val();
-    let arrayOfInput = [];
-    arrayOfInput.push(userWord);
+    const userWord = $("#userinput").val();
+    const arrayOfInput = [userWord];
+    console.log(arrayOfInput);
+    // arrayOfInput.push(userWord);
     const pigLatin = arrayOfInput.map(function (text) {
-      console.log(pigLatin);
-      if (/[aieou]/) {
-        arrayOfInput.push("way");
-        arrayOfInput.join((''));
+      let transformedText;
+      const firstLetterOfWord = text.split('')[0];
+      // const twoLetters = text.split('')[]
+      console.log("firstLetterOfWord", firstLetterOfWord);
+      // does first letter in text contain /[aieou]/
+      // 1. seperate out the first letter in text string
+      // 2. evalutate first letter against regex
+      if (firstLetterOfWord.match(/[aieou]/)) {
+        console.log("inside first block");
+        transformedText = text + 'way';
+        return transformedText
+      } else if (!firstLetterOfWord.match(/[aieou]/)) {
+        console.log("inside second block");
+        transformedText = text.split('');
+        console.log(transformedText);
+        transformedText.shift();
+        transformedText.push(firstLetterOfWord);
+        transformedText.push("ay");
+        transformedText = transformedText.join('');
+        console.log("second block transformed text", transformedText);
+        return transformedText;
       }
-      else if (/[^aieou]/i) {
-        arrayOfInput.replace(/[aieou]/gi, (charAt(0) + "ay"));
-      }
-      else if (/[qu]/gi) {
-        $(string).slice(+ "quay");
-      }
-
-      else if ("begins with y"); {
-      }
-      return (text);
     });
-    $("#translation").show();
   });
 });
